09c2b95b90741dc77a5a7f891028d5d36855e4b3
inoue_r <114901330+inoue021231@users.noreply.github.com>
2024-05-31 23:21:38 +0900

    Revert "Dev match"

--- src/components/match/Match.tsx
+++ src/components/match/Match.tsx
@@ -1,16 +1,10 @@
-import MatchIndicator from "./MatchIndicator";
-
 const Match = () => {
-  const gameTitle="Fall guys";
-  const appId = "438640";
   return (
     <div>
       <h3 className="text-lg font-semibold mb-2">一致度</h3>
       ここはしょうの作成ページです
-      <MatchIndicator appId={appId} gameTitle={gameTitle} />
     </div>
   )
 }
-
 
 export default Match
