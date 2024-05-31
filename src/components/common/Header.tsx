db2d99b71069aa46da197abb195fc2f42f020f35
kinakomoch7 <book-insect14@outlook.jp>
2024-05-31 23:40:35 +0900

    [add]link追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div className="h-14 rounded-none border-2 border-black">
       <h1>ヘッダー</h1>
+      <Link href="/network">
+        Home
+      </Link>
     </div>
   );
 }
