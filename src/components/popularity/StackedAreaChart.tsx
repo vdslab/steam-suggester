e2fcde9e372510494fd5dd996d9cf01bc0fdd6aa
k_tomoki <114901771+kinakomoch7@users.noreply.github.com>
2024-06-09 17:02:57 +0900

    [fix]縦のラベルを対応してなかった問題を修正

--- src/components/popularity/StackedAreaChart.tsx
+++ src/components/popularity/StackedAreaChart.tsx
@@ -5,13 +5,11 @@ import { scaleTime, scaleLinear, scaleOrdinal } from '@visx/scale';
 import { CountSteamReviews } from '@/types/Popularity/CountSteamReviews';
 import { StackedAreasProps } from '@/types/Popularity/StackedAreaProps';
 import { BG_COLOR_STACKED_AREA } from '@/constants/styles/stackedArea';
-import { AxisBottom, AxisLeft, AxisRight } from '@visx/axis';
+import { AxisBottom, AxisLeft } from '@visx/axis';
 
 const getX = (d: CountSteamReviews) => d.date;
-const getY0 = (d: SeriesPoint<CountSteamReviews>) => d[0] / 100;
-const getY1 = (d: SeriesPoint<CountSteamReviews>) => d[1] / 100;
-
-
+const getY0 = (d: SeriesPoint<CountSteamReviews>) => d[0];
+const getY1 = (d: SeriesPoint<CountSteamReviews>) => d[1];
 
 const StackedAreaChart =({
   data,
@@ -23,7 +21,10 @@ const StackedAreaChart =({
   const yMax = height - margin.top - margin.bottom;
   const xMax = width - margin.left - margin.right;
 
+
   const keys = Object.keys(data[0]).filter((k) => k !== 'date');
+
+  // console.log(data);
 
   const colorScale = scaleOrdinal<string, string>({
     domain: keys,
@@ -32,24 +33,27 @@ const StackedAreaChart =({
 
   const xScale = scaleTime<number>({
     range: [0, xMax],
-    domain: data ? [Math.min(...data.map((d) => d.date)), Math.max(...data.map((d) => d.date))] : [0, 0]
+    domain: [Math.min(...data.map((d) => d.date)), Math.max(...data.map((d) => d.date))]
   });
   const yScale = scaleLinear<number>({
     range: [yMax, 0],
+    domain: [0, Math.max(...data.map((d) => d.count))],
   });
+
+  console.log(xScale.domain());
 
   return width < 10 ? null : (
     <svg width={width+ 100} height={height +50}>
       {/* <GradientOrangeRed id="stacked-area-orangered" /> */}
-      <rect x={0} y={0} width={width} height={height} fill={BG_COLOR_STACKED_AREA} rx={14} />
-      <AxisBottom scale={xScale} label='時間(h)' top={yMax}/>
-      <AxisRight scale={yScale} label='人数' left={xMax}/>
+      <rect x={70} y={0} width={width} height={height} fill={BG_COLOR_STACKED_AREA} rx={14} />
+      <AxisBottom scale={xScale} label='時間(h)' top={yMax} left={70} />
+      <AxisLeft scale={yScale} label='レビュー数' left={70} top={0}/>
       <AreaStack
         top={margin.top}
         left={margin.left}
         keys={keys}
         data={data}
-        x={(d) => xScale(getX(d.data)) ?? 0}
+        x={(d) => xScale(getX(d.data)) + 70 ?? 0}
         y0={(d) => yScale(getY0(d)) ?? 0}
         y1={(d) => yScale(getY1(d)) ?? 0}
       >
