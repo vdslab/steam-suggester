6136f22fc762ae105bfad25a4fd32c41ab214acc
kerachino <tetucame@outlook.jp>
2024-06-14 00:17:52 +0900

    create api return steamGamesDetailsarray

--- src/app/api/popularity/getSteamGameDetail/route.ts
+++ src/app/api/popularity/getSteamGameDetail/route.ts
@@ -1,16 +1,24 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-  const GAME_ID = 438640;// 仮
+  const GAME_ID = 438640;
+  const gameNames = ["Apex", "Interstellar Distress"];
 
-  const res = await fetch(`https://store.steampowered.com/api/appdetails?appids=${GAME_ID}&cc=jp`)
-  const data = await res.json()
-  const result = {
-    name: data[`${GAME_ID}`].data.name,
-    genres: data[`${GAME_ID}`].data.genres,
-    categories: data[`${GAME_ID}`].data.categories,
-    priceOverview: parseInt(data[`${GAME_ID}`].data.price_overview.final_formatted.replace(/[^\d]/g, ''), 10)
+  const resultArray: any[] = [];
+  for(const game of gameNames){
+    const res = await fetch(`https://store.steampowered.com/api/storesearch/?term=${game}&cc=JP`)
+    const data = await res.json()
+    const gameId = data.items[0].id;
+
+    const res2 = await fetch(`https://store.steampowered.com/api/appdetails?appids=${gameId}&cc=jp`)
+    const data2 = await res2.json()
+    const result = {/*name: data2.items[0].name, */
+      name: data2[`${gameId}`].data.name,
+      genres: data2[`${gameId}`].data.genres,
+      categories: data2[`${gameId}`].data.categories
+    }
+    resultArray.push(result);
   }
 
-  return NextResponse.json(result)
+  return NextResponse.json(resultArray)
 }
