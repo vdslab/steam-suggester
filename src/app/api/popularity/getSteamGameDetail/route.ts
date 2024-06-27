ce187839aaf9ec0ecca69eb7e086e2289357f8e9
kerachino <tetucame@outlook.jp>
2024-06-27 23:22:43 +0900

    a bitta fixed

--- src/app/api/popularity/getSteamGameDetail/route.ts
+++ src/app/api/popularity/getSteamGameDetail/route.ts
@@ -25,10 +25,29 @@ export async function GET(req: Request) {
       const res2 = await fetch(`https://store.steampowered.com/api/appdetails?appids=${gameId}&cc=jp`);
       const data2 = await res2.json();
       if (data2[`${gameId}`]?.data) {
+        let setIsSinglePlayer = false;
+        const hasSinglePlayer = data2[`${gameId}`].data.categories.some(category => category.id === 2);
+        if (hasSinglePlayer) {
+          setIsSinglePlayer = true;
+        }
+
+        let setIsMultiPlayer = false;
+        const hasMultiPlayer = data2[`${gameId}`].data.categories.some(category => category.id === 1);
+        if (hasMultiPlayer) {
+          setIsMultiPlayer = true;
+        }
+
         const result = {
-          name: data2[`${gameId}`].data.name,
-          genres: data2[`${gameId}`].data.genres,
-          categories: data2[`${gameId}`].data.categories
+          steamGameID: gameId,
+          title: data2[`${gameId}`].data.name,
+          imgURL: data2[`${gameId}`].data.header_image,
+   
+            genres: data2[`${gameId}`].data.genres,
+            categories: data2[`${gameId}`].data.categories,
+            isSinglePlayer: setIsSinglePlayer,
+            isMultiPlayer: setIsMultiPlayer
+            // price: data2[`${gameId}`].data.categories[0].id
+          
         };
         resultArray.push(result);
       }
