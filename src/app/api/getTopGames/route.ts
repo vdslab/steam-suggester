79c4b40e9dbeee79dfd08b44c8ead7e72796a994
kinakomoch7 <book-insect14@outlook.jp>
2024-06-30 04:38:26 +0900

    [update]APIでDBからトークン取得に変更

--- src/app/api/getTopGames/route.ts
+++ src/app/api/getTopGames/route.ts
@@ -4,41 +4,56 @@ import { NextResponse } from 'next/server';
 export async function GET(req: Request) {
   const TWITCH_API_URL = 'https://api.twitch.tv/helix/games/top?first=100'
   
-  const headers = new Headers();
-  if (process.env.TWITCH_CLIENT_ID && process.env.TWITCH_ACCESS_TOKEN) {
-    headers.append('Client-ID', process.env.TWITCH_CLIENT_ID);
-    headers.append('Authorization', `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`);
-  } else {
+  try {
+
+    // TwitchのAPIにアクセスするためのトークンを取得
+    const tokenResponse = await fetch(`${process.env.CURRENT_URL}/api/getTwitchToken`);
+    if (!tokenResponse.ok) {
+      return NextResponse.error()
+    }
+    const token = await tokenResponse.json();
+
+    // TwitchのAPIにアクセスするためのヘッダーを作成
+    const headers = new Headers();
+    if (process.env.TWITCH_CLIENT_ID && token) {
+      headers.append('Client-ID', process.env.TWITCH_CLIENT_ID);
+      headers.append('Authorization', `Bearer ${token}`);
+    } else {
+      return NextResponse.error()
+    }
+
+
+    // // 1~100件目のデータを取得
+    const firstRes = await fetch(TWITCH_API_URL,{headers: headers,})
+    const firstData = await firstRes.json()
+    const pageCursor = firstData.pagination.cursor
+    const firstGames = firstData.data.map((game: TwitchTopGameType) => {
+      return ({
+        twitchGameId: game.id,
+        title: game.name,
+      })
+    })
+
+    // 100~200件目のデータを取得
+    const secondRes = await fetch(
+      `${TWITCH_API_URL}&after=${pageCursor}`,
+      {
+        headers: headers,
+      }
+    )
+    const secondData = await secondRes.json()
+    const secondGames = secondData.data.map((game: TwitchTopGameType) => {
+      return ({
+        twitchGameId: game.id,
+        title: game.name,
+      })
+    })
+
+    const games = firstGames.concat(secondGames)
+
+    return NextResponse.json(games)
+  } catch (error) {
+    console.error('Error fetching top games:', error)
     return NextResponse.error()
   }
-
-  // 1~100件目のデータを取得
-  const firstRes = await fetch(TWITCH_API_URL,{headers: headers,})
-  const firstData = await firstRes.json()
-  const pageCursor = firstData.pagination.cursor
-  const firstGames = firstData.data.map((game: TwitchTopGameType) => {
-    return ({
-      twitchGameId: game.id,
-      title: game.name,
-    })
-  })
-
-  // 100~200件目のデータを取得
-  const secondRes = await fetch(
-    `${TWITCH_API_URL}&after=${pageCursor}`,
-    {
-      headers: headers,
-    }
-  )
-  const secondData = await secondRes.json()
-  const secondGames = secondData.data.map((game: TwitchTopGameType) => {
-    return ({
-      twitchGameId: game.id,
-      title: game.name,
-    })
-  })
-
-  const games = firstGames.concat(secondGames)
-
-  return NextResponse.json(games)
 }
--- src/app/api/getTwitchToken/route.ts
+++ src/app/api/getTwitchToken/route.ts
@@ -1,11 +1,14 @@
 import { PG_POOL } from '@/constants/PG_POOL';
 import { NextResponse } from 'next/server';
 
-
 export async function GET() {
   try {
     const client = await PG_POOL.connect();
-    const result = await client.query('SELECT token FROM access_token ORDER BY get_date DESC LIMIT 1', []);
+    const result = await client.query(
+      'SELECT token FROM access_token WHERE client_id = $1 ORDER BY get_date DESC LIMIT 1',
+      [process.env.TWITCH_CLIENT_ID]
+    );
+
     await client.release(true);
 
     if (result.rows.length > 0) {
