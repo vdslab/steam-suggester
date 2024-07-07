75c4fcd1c968512049e3d64a3da5c2770c35ecc3
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-07-07 15:10:22 +0900

    日付が変わったので昨日のデータを取得し、現在のランキングデータを取得するように変更

--- src/app/api/getCurrentTopGames/route.ts
+++ src/app/api/getCurrentTopGames/route.ts
@@ -4,8 +4,7 @@ import { PG_POOL } from "@/constants/PG_POOL";
 export async function GET(req: Request) {
   try {
     const today = new Date();
-    // 7/7時点で昨日のデータがないため本日のデータ取得に一時変更
-    // today.setDate(today.getDate() - 1); 
+    today.setDate(today.getDate() - 1); 
     const dateString = today.toISOString().split('T')[0];
 
     const query = `
