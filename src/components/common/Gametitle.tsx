7aeddce8f8b7d70fe4490dd32be479f79a275277
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-07-07 15:16:32 +0900

    ゲームタイトルコンポーネントをサーバーサイドに変更

--- src/components/common/Gametitle.tsx
+++ src/components/common/Gametitle.tsx
@@ -1,22 +1,12 @@
-'use client';
-
-import { useEffect, useState } from "react";
-
-const Gametitle = (props: {steamGameId:string}) => {
+const Gametitle = async (props: {steamGameId:string}) => {
   const { steamGameId } = props;
-  const [title, setTitle] = useState<string>("");
-  
-  useEffect(() => {
-    (async() => {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_CURRENT_URL}/api/getMatchDetails/${steamGameId}/0`);
-      const data = await response.json();
-      setTitle(data.title);
-    })();
-    
-  },[]);
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_CURRENT_URL}/api/getMatchDetails/${steamGameId}/0`);
+  const data = await response.json();
+
 
   return (
-    <h2 className="text-xl font-semibold mb-4 text-white text-center">{title}</h2>
+    <h2 className="text-xl font-semibold mb-4 text-white text-center">{data.title}</h2>
   );
 }
 
