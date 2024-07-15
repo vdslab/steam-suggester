0cb3913273e6bfe7618e327b816e9ce84135bddb
kinakomoch7 <book-insect14@outlook.jp>
2024-07-15 20:49:24 +0900

    [fix]修正によってnetworkボタンが機能してなかったので修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="h-[8dvh] leading-[8dvh] bg-[#171a21] flex text-3xl text-white space-x-5 pl-8">
       <div>{TITLE}</div>
-      <Link href="/network">
+      <Link href="/">
         network
       </Link>
     </div>
