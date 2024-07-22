16035f9ddb66f9dc03f38a0a99d63ddfa19270d0
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-07-22 17:52:58 +0900

    ノードリンク図上のスクロールバーを削除

--- src/components/network/Network.tsx
+++ src/components/network/Network.tsx
@@ -4,7 +4,6 @@ import NodeLink from "./NodeLink";
 import SelectParameter from './SelectParameter';
 
 const Network = () => {
-
   const [filter, setFilter] = useState({
     Categories: {
       1: true,
@@ -46,8 +45,8 @@ const Network = () => {
       isMultiPlayer: true
     },
     Device: {
-        windows: true,
-        mac: true
+      windows: true,
+      mac: true
     },
     Playtime: {
       1: true,
@@ -62,16 +61,17 @@ const Network = () => {
       10: true,
     },
   });
+
   return (
-    <div className="flex h-[92dvh]">
-      <div className="w-1/4 bg-[#1b2838]">
+    <div className="flex h-[92dvh] overflow-hidden">
+      <div className="w-1/4 bg-[#1b2838] overflow-y-auto overflow-x-hidden">
         <SelectParameter filter={filter} setFilter={setFilter} />
       </div>
-      <div className="w-3/4 bg-[#2a475e] flex flex-col p-4">
-        <NodeLink filter={filter}/>
+      <div className="w-3/4 bg-[#2a475e] flex flex-col overflow-y-hidden overflow-x-hidden">
+        <NodeLink filter={filter} />
       </div>
     </div>
-  )
+  );
 }
 
 export default Network;
