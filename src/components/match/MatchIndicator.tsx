bc30915bd8c4c01670a2aeee5e1e36e097e2e9a3
kerachino <tetucame@outlook.jp>
2024-07-25 22:25:08 +0900

    deal with price no data&

--- src/components/match/MatchIndicator.tsx
+++ src/components/match/MatchIndicator.tsx
@@ -52,7 +52,7 @@ const MatchIndicator = ( props:Props ) => {
         <p className="text-lg">ジャンル一致度</p>
         <PercentBar baseStyle='bg-match-genre-bg rounded-t-lg' txtStyle='text-match-genre' percent={genreMatchPercentage} />
         
-        <div className="w-full bg-gray-300 rounded-b-lg pt-0 pb-1 pl-2 pr-1 ">
+        <div className="w-full bg-gray-300 rounded-b-lg pt-0.5 pb-1 pl-2 pr-1 ">
           {data.genres.map((genre) => (
             <div key={genre.id} className="inline-block bg-white text-gray-700 rounded-md px-2 py-0 my-0.5 cursor-pointer">
               <small>{genre.description}</small>
@@ -63,26 +63,32 @@ const MatchIndicator = ( props:Props ) => {
 
       <div className="mb-4">
         <p>価格(円)</p>
-        <div className="relative w-full h-8 bg-gray-200 rounded-lg mb-1 mt-4">
-          {data.price ? (
-            data.price != 0 ? (
-              <div>
-                <div
-                  className={`absolute top-0 left-0 h-full rounded-lg bg-orange-400`}
-                  style={{
-                    width: `${priceBarPosition(data.price)}%`,
-                  }}></div>
-                
-                <LocalFilterPrice startPrice={localFilter.Price.startPrice} endPrice={localFilter.Price.endPrice} startPricePosition={startPricePosition} endPricePosition={endPricePosition} />
-                <div className="absolute top-0 left-0 transform -translate-x-1/2 h-full w-0.5 bg-black"></div>
-                <div className="absolute top-0 right-0 transform translate-x-1/2 h-full w-0.5 bg-black"></div>
-                <div className={`absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold text-orange-700`}>
-                  {data.price.toLocaleString()}
+          {data.price != 0 ? (
+            <div>
+              <div className="relative w-full h-8 bg-gray-200 rounded-lg mb-1 mt-4">
+                <div>
+                  <div
+                    className={`absolute top-0 left-0 h-full rounded-lg bg-orange-400`}
+                    style={{
+                      width: `${priceBarPosition(data.price)}%`,
+                    }}></div>
+                  
+                  <LocalFilterPrice startPrice={localFilter.Price.startPrice} endPrice={localFilter.Price.endPrice} startPricePosition={startPricePosition} endPricePosition={endPricePosition} />
+                  <div className="absolute top-0 left-0 transform -translate-x-1/2 h-full w-0.5 bg-black"></div>
+                  <div className="absolute top-0 right-0 transform translate-x-1/2 h-full w-0.5 bg-black"></div>
+                  <div className={`absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold text-orange-700`}>
+                    {data.price.toLocaleString()}
+                  </div>
                 </div>
               </div>
+              <div className="flex justify-between text-xs">
+                <span>0</span>
+                <span>10,000</span>
+              </div>
+            </div>
             ) : (
               <div
-                className="h-4 rounded-lg bg-gray-400 flex items-center justify-center text-white text-xs"
+                className="h-8 rounded-lg bg-gray-400 flex items-center justify-center text-lg font-bold text-white"
                 style={{
                   width: '100%',
                 }}
@@ -90,23 +96,7 @@ const MatchIndicator = ( props:Props ) => {
                 無料
               </div>
             )
-          ):
-            <div
-              className="h-full rounded-lg bg-gray-400 flex items-center justify-center text-white text-xs"
-              style={{
-                width: '100%',
-              }}
-            >
-              0円
-            </div>
           }
-        </div>
-        {data.price && data.price != 0 ? (
-          <div className="flex justify-between text-xs">
-            <span>0</span>
-            <span>10,000</span>
-          </div>
-        ) : null}
       </div>
 
       <div className="mb-4">
