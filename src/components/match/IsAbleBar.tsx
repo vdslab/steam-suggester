fc4877d1a7230e7bc2506f16067d886269b40fa5
kerachino <tetucame@outlook.jp>
2024-07-26 00:21:45 +0900

    on playmode of match func, green

--- src/components/match/IsAbleBar.tsx
+++ src/components/match/IsAbleBar.tsx
@@ -16,11 +16,11 @@ const IsAbleBar = (props:Props) => {
   return (
     <div className="flex">
       <span
-        className={`flex-1 px-2 py-1 rounded cursor-pointer text-center border-b-4 ${isUserLeft ? 'border-green-500' : isLeft ? 'border-yellow-200' : 'border-gray-400'} ${isLeft ? 'bg-yellow-200 text-yellow-800' : 'bg-gray-400 text-yellow-800'}`}>
+        className={`flex-1 px-2 py-1 rounded cursor-pointer text-center border-b-4 ${isUserLeft ? 'border-green-500' : isLeft ? 'border-green-200' : 'border-gray-400'} ${isLeft ? 'bg-green-200 text-yellow-800' : 'bg-gray-400 text-green-800'}`}>
         {leftTxt}
       </span>
       <span
-        className={`flex-1 px-2 py-1 rounded cursor-pointer text-center border-b-4 ${isUserRight ? 'border-green-500' : isRight ? 'border-yellow-200' : 'border-gray-400'} ${isRight ? 'bg-yellow-200 text-yellow-800' : 'bg-gray-400 text-yellow-800'}`}>
+        className={`flex-1 px-2 py-1 rounded cursor-pointer text-center border-b-4 ${isUserRight ? 'border-green-500' : isRight ? 'border-green-200' : 'border-gray-400'} ${isRight ? 'bg-green-200 text-yellow-800' : 'bg-gray-400 text-green-800'}`}>
         {rightTxt}
       </span>
     </div>
