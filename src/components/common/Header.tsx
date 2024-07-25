30b635314bceb7da78f9e1f462006791bf061c7c
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-07-25 16:35:52 +0900

    データ取得先をSteam APIからデータベースへ変更

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="h-[8dvh] leading-[8dvh] bg-[#171a21] flex text-3xl text-white space-x-5 pl-8 justify-between">
-      <div>SteamSuggester</div>
+      <div>Steam Suggester</div>
       <Link href="/" className="pr-10 pl-10 hover:bg-[#22262e]">
         network
       </Link>
