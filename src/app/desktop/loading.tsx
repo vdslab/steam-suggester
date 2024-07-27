06c6aa08e4a8d44c6e2aa6ed7bd49b7029cc68de
kinakomoch7 <book-insect14@outlook.jp>
2024-07-27 12:17:29 +0900

    [fix]反転削除

--- src/app/desktop/loading.tsx
+++ src/app/desktop/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-  <div className='flex space-x-2 justify-center items-center h-screen dark:invert bg-gray-900'>
+  <div className='flex space-x-2 justify-center items-center h-screen bg-gray-900'>
     <span className='sr-only'>Loading...</span>
       <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
     <div className='h-8 w-8 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
