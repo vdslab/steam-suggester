0ce024e0e083d74394e697b16c5ec5587dc8119e
kerachino <tetucame@outlook.jp>
2024-11-23 00:01:28 +0900

    create streamedList(delete part of import something in streamedList.tsx

--- src/components/network/streamedList/StreamedList.tsx
+++ src/components/network/streamedList/StreamedList.tsx
@@ -2,9 +2,6 @@ import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import { NodeType, StreamerListType } from "@/types/NetworkType";
-import { useCallback, useEffect, useState } from "react";
-import similarity from 'string-similarity';
 
 
 
