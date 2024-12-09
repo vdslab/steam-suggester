c0d7fc1775b804a70ecfdc5fadda898ba8d5a76d
kerachino <tetucame@outlook.jp>
2024-12-09 19:31:19 +0900

    fixed layout bag in gamelist, changed network height to 100%

--- src/components/network/Panel.tsx
+++ src/components/network/Panel.tsx
@@ -11,12 +11,12 @@ type PanelProps = {
 
 const Panel: React.FC<PanelProps> = ({ title, icon, children }) => {
   return (
-    <div className="flex-1 h-[95vh] bg-gray-800 rounded-r-lg p-4 shadow-md flex flex-col space-y-2">
+    <div className="flex-1 bg-gray-800 rounded-r-lg p-4 shadow-md flex flex-col space-y-2 h-full">
       <div className="flex items-center space-x-2">
         {icon}
         <h2 className="text-white text-lg font-semibold">{title}</h2>
       </div>
-      <div className="border-t border-gray-700 pt-2">
+      <div className="border-t border-gray-700 pt-2 flex-1 overflow-y-auto">
         {children}
       </div>
     </div>
