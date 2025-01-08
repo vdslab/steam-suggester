a1918c1531a7b657f9bcd324484b56152f0ed574
kerachino <tetucame@outlook.jp>
2025-01-08 22:59:58 +0900

    gameSearch moved in right of newtworkComponent

---
+++ src/components/network/gameDetail/gameDetail.tsx
@@ -0,0 +1,224 @@
+/* GameSearchPanel.tsx */
+"use client";
+
+import { useEffect, useRef } from "react";
+import { NodeType, SteamListType } from "@/types/NetworkType";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SearchIcon from "@mui/icons-material/Search";
+import Section from "../Section";
+import HelpTooltip from "../HelpTooltip";
+import AppleIcon from "@mui/icons-material/Apple";
+import PersonIcon from "@mui/icons-material/Person";
+import GroupIcon from "@mui/icons-material/Group";
+import Tooltip from "@mui/material/Tooltip";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import WindowsIcon from "@/components/common/WindowsIcon";
+import ToggleDisplay from "./ToggleDisplay";
+import Link from "next/link";
+import Popularity from "./Popularity2";
+import ReviewCloud from "../../charts/ReviewCloud";
+import { SteamDetailsDataType } from "@/types/api/getSteamDetailType";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+
+import BuildIcon from "@mui/icons-material/Build";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+
+type Props = {
+  nodes: NodeType[];
+  selectedIndex: number;
+  setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
+  setIsNetworkLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  steamListData: SteamListType[];
+  steamAllData: SteamDetailsDataType[];
+};
+
+const GameSearchPanel = (props: Props) => {
+  const {
+    steamListData,
+    nodes,
+    selectedIndex,
+    setSelectedIndex,
+    setIsNetworkLoading,
+    steamAllData,
+  } = props;
+  const router = useRouter();
+
+  // Ref for the selected game detail
+  const selectedDetailRef = useRef<HTMLDivElement | null>(null);
+
+  // 自動スクロール機能の追加
+  useEffect(() => {
+    if (selectedIndex !== -1 && selectedDetailRef.current) {
+      selectedDetailRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [selectedIndex]);
+
+  return (
+    <div className="flex-1 bg-gray-800 rounded-l-lg shadow-md flex flex-col space-y-4 overflow-y-scroll h-full relative">
+      {/* 選択されたゲームの詳細表示 */}
+      {selectedIndex !== -1 && nodes[selectedIndex] ? (
+        <div className="rounded-lg" ref={selectedDetailRef}>
+          {/* ゲーム詳細内容 */}
+          <div className="flex flex-col space-y-2">
+            {/* ゲーム画像 */}
+            {nodes[selectedIndex].imgURL && (
+              <Image
+                src={nodes[selectedIndex].imgURL}
+                alt={nodes[selectedIndex].title}
+                width={600}
+                height={400}
+                style={{ borderRadius: "4px" }}
+                className="object-cover rounded mb-2"
+              />
+            )}
+            <div className="px-2">
+              <div className="flex items-center">
+                {/* ゲームタイトル */}
+                <h2 className="text-white text-xl font-semibold">
+                  {nodes[selectedIndex].title}
+                </h2>
+
+                {/* Steamリンク */}
+                <Tooltip title="Steamで開く">
+                  <Link
+                    href={`https://store.steampowered.com/app/${nodes[selectedIndex].steamGameId}/`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="ml-2 text-blue-400"
+                  >
+                    <OpenInNewIcon fontSize="small" />
+                  </Link>
+                </Tooltip>
+              </div>
+
+              {/* 開発者、発売日、価格 */}
+              <div className="flex items-center text-xs space-x-4 mt-1 mb-2">
+                {/* 開発者 */}
+                <div className="flex items-center">
+                  <BuildIcon fontSize="small" className="text-gray-400 mr-1" />
+                  <span className="text-gray-300">
+                    {nodes[selectedIndex].developerName}
+                  </span>
+                </div>
+
+                {/* 発売日 */}
+                <div className="flex items-center">
+                  <CalendarTodayIcon
+                    fontSize="small"
+                    className="text-gray-400 mr-1"
+                  />
+                  <span className="text-gray-300">
+                    {nodes[selectedIndex].releaseDate}
+                  </span>
+                </div>
+
+                {/* 価格 */}
+                <div className="flex items-center">
+                  <AttachMoneyIcon
+                    fontSize="small"
+                    className="text-gray-400 mr-1"
+                  />
+                  <span className="text-gray-300">
+                    {nodes[selectedIndex].salePrice &&
+                    parseInt(nodes[selectedIndex].salePrice, 10) <
+                      nodes[selectedIndex].price ? (
+                      <>
+                        <span className="line-through text-gray-400">
+                          ¥{nodes[selectedIndex].price}
+                        </span>
+                        <span className="text-red-500 ml-1">
+                          ¥{nodes[selectedIndex].salePrice}
+                        </span>
+                      </>
+                    ) : nodes[selectedIndex].price ? (
+                      `¥${nodes[selectedIndex].price}`
+                    ) : (
+                      "無料"
+                    )}
+                  </span>
+                </div>
+              </div>
+
+              {/* アイコン表示 */}
+              <div className="flex items-center space-x-1 mt-1 mb-2">
+                {/* デバイスサポート */}
+                {nodes[selectedIndex].device.windows && (
+                  <Tooltip title="Windows対応">
+                    <WindowsIcon size={20} />
+                  </Tooltip>
+                )}
+                {nodes[selectedIndex].device.mac && (
+                  <Tooltip title="Mac対応">
+                    <AppleIcon className="text-white h-5 w-5" />
+                  </Tooltip>
+                )}
+
+                {/* マルチプレイヤー情報 */}
+                {nodes[selectedIndex].isSinglePlayer && (
+                  <Tooltip title="Single Player">
+                    <PersonIcon className="text-white h-5 w-5" />
+                  </Tooltip>
+                )}
+                {nodes[selectedIndex].isMultiPlayer && (
+                  <Tooltip title="Multiplayer">
+                    <GroupIcon className="text-white h-5 w-5" />
+                  </Tooltip>
+                )}
+              </div>
+
+              {/* ゲーム説明文 */}
+              <div>
+                {nodes[selectedIndex].shortDetails && (
+                  <div className="text-white text-sm mt-1">
+                    {nodes[selectedIndex].shortDetails}
+                  </div>
+                )}
+              </div>
+
+              {/* タグ */}
+              <div className="text-white">
+                {nodes[selectedIndex].tags &&
+                  nodes[selectedIndex].tags.length > 0 && (
+                    <div className="line-clamp-2 overflow-hidden">
+                      {nodes[selectedIndex].tags.map((tag, index) => (
+                        <span
+                          key={index}
+                          className="bg-green-500 text-xs p-0.5 mr-1 mb-1 rounded inline-block whitespace-nowrap cursor-pointer select-none"
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
+                  )}
+              </div>
+            </div>
+          </div>
+
+          {/* レビュー分析タイトルの追加 */}
+          <div className="px-2 mt-4">
+            <h3 className="text-white text-lg font-semibold mb-2">
+              レビュー分析
+            </h3>
+            <ReviewCloud steamData={steamAllData[selectedIndex]} />
+          </div>
+
+          {/* Populatityコンポーネント */}
+          <Popularity nodes={nodes} selectedIndex={selectedIndex} />
+        </div>
+      ) : (
+        <div className="text-white text-center pt-24">
+          ゲームを選択してください。
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GameSearchPanel;
