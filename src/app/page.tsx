ad8dddc65eed66ccd0a83c076dbd1aef83c70e7f
kinakomoch7 <book-insect14@outlook.jp>
2025-01-10 19:04:05 +0900

    [add]あとでフロント側で取得にします

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,29 @@ import { SteamDetailsDataType } from "@/types/api/getSteamDetailType";
 import { SteamListType } from "@/types/NetworkType";
 
 export default async function Page() {
-  const matchRes = await fetch(
-    `${process.env.NEXT_PUBLIC_CURRENT_URL}/api/network/getMatchGames`
-  );
-  const steamAllData: SteamDetailsDataType[] = await matchRes.json();
+  try {
+    // データ取得
+    const matchRes = await fetch(
+      `${process.env.NEXT_PUBLIC_CURRENT_URL}/api/network/getMatchGames`
+    );
+    const steamAllData: SteamDetailsDataType[] = await matchRes.json();
 
-  const steamListRes = await fetch(
-    `${process.env.NEXT_PUBLIC_CURRENT_URL}/api/network/getSteamList`
-  );
-  const steamListData: SteamListType[] = await steamListRes.json();
+    const steamListRes = await fetch(
+      `${process.env.NEXT_PUBLIC_CURRENT_URL}/api/network/getSteamList`
+    );
+    const steamListData: SteamListType[] = await steamListRes.json();
 
-  return (
-    <div className="flex flex-col h-screen">
-      <Network steamAllData={steamAllData} steamListData={steamListData} />
-    </div>
-  );
+    return (
+      <div className="flex flex-col h-screen">
+        <Network steamAllData={steamAllData} steamListData={steamListData} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <div className="flex flex-col h-screen">
+        <p>データの取得に失敗しました。</p>
+      </div>
+    );
+  }
 }
