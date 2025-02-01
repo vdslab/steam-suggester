0ef0b9efc5dd95ab2e413d0b8a08bebdb7c0d7d8
inoue_r <chru21072@g.nihon-u.ac.jp>
2025-02-01 10:50:40 +0900

    タイトル？？？

--- src/components/common/Headers.tsx
+++ src/components/common/Headers.tsx
@@ -1,9 +1,7 @@
 export const HomeHeader = () => {
   return (
     <header className="absolute top-0 w-full h-[8vh] bg-gradient-to-b from-gray-800 to-transparent text-white flex items-center justify-center z-10 select-none">
-      <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text animate-gradient">
-        Steam Suggester
-      </h1>
+      <h1 className="text-4xl font-bold">Steam Suggester</h1>
     </header>
   );
 };
